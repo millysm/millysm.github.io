2de92683206f5005d58a5cdf8beb516170954c76
millysm <48333620+millysm@users.noreply.github.com>
2019-03-07 15:49:14 -0500

    Add files via upload

---
+++ scripts/dressup.js
@@ -0,0 +1,31 @@
+function allowDrop(ev) {
+	ev.preventDefault()
+}
+function drag(ev) {
+	ev.dataTransfer.setData('text', ev.target.id)
+
+}
+function drop(ev) {
+	ev.preventDefault()
+	var data = ev.dataTransfer.getData('text')
+	ev.target.removeChild(document.getElementById(data))
+}
+
+function changeImage(ev, imageUrl) {
+	ev.preventDefault()
+	
+	var img = document.createElement("img")
+	img.src = imageUrl
+
+	var src = document.getElementById("frog");
+	src.appendChild(img);
+	
+	img.style.position = "absolute"
+	img.style.bottom = "-338px"
+	img.style.width = "650px"
+	
+	/* THIS ONLY WORKS IN FIREFOX */
+	var data = ev.dataTransfer.getData('text')
+	document.getElementById(data).style.opacity = "0.0"
+}
+
