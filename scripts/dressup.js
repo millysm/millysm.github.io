3958c09902446bbb66204f8555047b37344c6921
millysm <48333620+millysm@users.noreply.github.com>
2019-03-09 23:50:21 -0600

    misha update 3/9
    updated javascript to make it easier to position the clothes

--- scripts/dressup.js
+++ scripts/dressup.js
@@ -11,21 +11,12 @@ function drop(ev) {
 	ev.target.removeChild(document.getElementById(data))
 }
 
-function changeImage(ev, imageUrl) {
+function changeImage(ev, imageId) {
 	ev.preventDefault()
 	
-	var img = document.createElement("img")
-	img.src = imageUrl
-
-	var src = document.getElementById("frog");
-	src.appendChild(img);
-	
-	img.style.position = "absolute"
-    img.style.right = "450px"
-	img.style.width = "650px"
-	
+	document.getElementById(imageId).style.visibility = "visible"
+    
 	/* THIS ONLY WORKS IN FIREFOX */
 	var data = ev.dataTransfer.getData('text')
-	document.getElementById(data).style.opacity = "0.0"
+	document.getElementById(data).style.display = "none"
 }
-
