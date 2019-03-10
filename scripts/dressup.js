ae81744da8417b2dc0491fe191712fe48ac05216
millysm <48333620+millysm@users.noreply.github.com>
2019-03-10 16:31:47 -0500

    bHUHUMNBJG

--- scripts/dressup.js
+++ scripts/dressup.js
@@ -1,22 +1,22 @@
 function allowDrop(ev) {
-	ev.preventDefault()
+	ev.preventDefault();
 }
 function drag(ev) {
-	ev.dataTransfer.setData('text', ev.target.id)
+	ev.dataTransfer.setData('text', ev.target.id);
 
 }
 function drop(ev) {
-	ev.preventDefault()
-	var data = ev.dataTransfer.getData('text')
-	ev.target.removeChild(document.getElementById(data))
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData('text');
+	ev.target.removeChild(document.getElementById(data));
 }
 
 function changeImage(ev, imageId) {
-	ev.preventDefault()
+	ev.preventDefault();
 	
-	document.getElementById(imageId).style.visibility = "visible"
+	document.getElementById(imageId).style.visibility = "visible";
     
 	/* THIS ONLY WORKS IN FIREFOX */
-	var data = ev.dataTransfer.getData('text')
-	document.getElementById(data).style.display = "none"
+	var data = ev.dataTransfer.getData('text');
+	document.getElementById(data).style.display = "none";
 }
